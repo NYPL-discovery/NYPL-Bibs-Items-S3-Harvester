6332027bc3e241eec9e452a35dece4ff3497c2a8
Jobin Thomas <jobinthomas@nypl.org>
2017-08-22 09:49:15 -0400

    Read string as UTF-8 from exchange Body

--- src/main/java/org/nypl/s3/nyplrecords/harvester/routes/S3HarvesterRouter.java
+++ src/main/java/org/nypl/s3/nyplrecords/harvester/routes/S3HarvesterRouter.java
@@ -96,7 +96,7 @@ public class S3HarvesterRouter extends RouteBuilder {
                   if (bibAvroSchema.getSchema() == null) {
                     bibAvroSchema.setSchema(retryTemplate, producerTemplate);
                   }
-                  String nyplRecord = exchange.getIn().getBody(String.class);
+                  String nyplRecord = new String(exchange.getIn().getBody(String.class).getBytes("UTF-8"));
                   logger.info(nyplRecord);
                   Map<String, Object> nyplRecordKeyVals =
                       new ObjectMapper().readValue(nyplRecord, Map.class);
